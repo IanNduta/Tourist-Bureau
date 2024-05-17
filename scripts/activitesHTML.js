ada2ef48d64c9392ad683beb43aae81cdd1a692b
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-17 11:55:06 -0400

    finished creating my form page

--- scripts/activitesHTML.js
+++ scripts/activitesHTML.js
@@ -1,1 +1,18 @@
 "use strict";
+//inputs
+//Dropdown options
+const activitylCategory = document.getElementById("activitylCategory");
+const activityNames = document.getElementById("activityNames");
+//Inputs
+const numOfTickets = document.getElementById("numOfTickets");
+const cardNumber = document.getElementById("cardNumber");
+const emailAddress = document.getElementById("emailAddress");
+//Buttons
+const submitBtn = document.getElementById("submitBtn");
+const resetBtn = document.getElementById("resetBtn");
+//ouputs
+const customerId = document.getElementById("customerId");
+const nameOfActivity = document.getElementById("nameOfActivity");
+const activityDescription = document.getElementById("activityDescription");
+const activityLocation = document.getElementById("activityLocation");
+const priceOfActivity = document.getElementById("priceOfActivity");
